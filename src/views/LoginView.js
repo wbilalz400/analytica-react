68d1cc657ff52633cc14b67f7c3d5a21637b0c51
wbilalz400 <bilalzahid1997@hotmail.com>
2020-05-17 02:48:16 +0500

    Incomplete Login Page

---
+++ src/views/LoginView.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container';
+
+const styles = {
+    leftPane : {
+        height:'100vh', 
+        background: "rgb(2,0,36)" ,
+        background:' linear-gradient(171deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)',
+        alignItems: 'center',
+        justifyContent: 'center',
+        display: 'flex',
+    },
+    card : {
+        marginRight: '25%', 
+        marginLeft: '25%',
+        background: 'white',
+        border: '5px',
+        borderColor: 'white',
+        borderRadius : '20px',
+        height: '60%',
+        boxShadow: "10px 10px 16px -5px rgba(0,0,0,0.75)"
+
+    }
+}
+ 
+
+export default class LoginView extends React.Component {
+
+    render() {
+
+        return (
+            <div>
+            <Row>
+            <Col style={styles.leftPane}>
+                <Container style={styles.card}>
+                
+                </Container>
+            </Col>
+            <Col>2 of 2</Col>
+            </Row>
+            </div>
+        );
+    }
+}
