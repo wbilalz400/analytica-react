eaf9cab367a483981044bf8bb39e5972390bfbb7
Aaamir Afzal <aamirafzal9669@gmail.com>
2020-05-18 17:10:33 +0500

    Updated Navbar Header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,49 @@
 import React from 'react'
-import logo from './logo.png'
+import logo from '../images/logo2.png'
 import user from '../images/user.png'
+import light from '../images/thunder.png'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container';
+
+
 function Navbar() {
     return (
-        <nav className='navbar'>
+            <Row className='row'>
+                <Col className='col1'>
+                
+                   <h1><img src={logo}></img>Analytica.IoT
+                    </h1>
             
-            <h1>  <img src={logo} alt='ANALYTICA.IoT'/>Aalytica.IoT  <img src={logo}/></h1>
+                    <img className='img1' src={light}/>
+                  
+                </Col>
+                <Col className='col2'>
+                    <ul>
+                        <li><a href=''>About</a></li>
+                        <li><a href=''>Pricing</a></li>
+                        <li><a href=''>Dashboards</a></li>
+                        <li><a href=''>Resources</a></li>
+                        <li><a href=''>Platforms</a></li>
+                    </ul>
+                </Col>
+                <Col className='col3'>
+                <button>Demo</button>
+                <a><img src={user}></img></a>
+                
+                </Col>
+           </Row>
+
             
-            <a href="https://www.w3schools.com">
-            <img src={user}/>
-            </a>
-            <button>Demo</button>
+           
+            
+           
+            
            
             
           
             
-        </nav>
+        
     )
 }
 export default Navbar;
