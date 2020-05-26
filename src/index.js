c5906c112d5cf7aee1c18fcfc91d3da40cdec51b
Aaamir Afzal <aamirafzal9669@gmail.com>
2020-05-26 17:15:25 +0500

    Minnor Fixes

--- src/index.js
+++ src/index.js
@@ -15,19 +15,14 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeView from './views/HomeView';
 
-const images = [
-  'https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
-  'https://images.unsplash.com/photo-1470341223622-1019832be824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2288&q=80',
-  'https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2094&q=80',
-  'https://images.unsplash.com/photo-1534161308652-fdfcf10f62c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2174&q=80'
-]
+
 
 ReactDOM.render(
   <React.StrictMode>
   <Router>
     <Switch>
       <div>
-        <Route exact path="/" component={Slider}/>
+        <Route exact path="/" component={App}/>
         <Route exact path="/login" component={LoginView}/>
         <Route exact path="/register" component={RegisterView}/>
         <Route exact path="/home" component={HomeView}/>
